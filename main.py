d5df84e89497399f4abccaccf34f308c61161fbb
Lukasz Wasilewski <lukasz.wasilewski.7@gmail.com>
2023-08-30 16:38:37 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
+
+FORMSPREE = os.environ.get("FORMSPREE")
+
+@app.route('/')
+def home():
+    return render_template("index.html", site=FORMSPREE)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
